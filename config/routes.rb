4fb2fbd35a79af569c5d9a8472bd6861e50e46f0
Oguz Karahanoglu <karahanogluoguz@gmail.com>
2021-05-20 16:17:48 +0200

    watch list

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
+  get 'lists/new'
+  get 'lists/create'
+  get 'lists/index'
+  get 'lists/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists do
+    resources :bookmarks, only: [:new, :create, :destroy]
+  end
+
+  resources :lists, only: [:new, :create, :index, :show]
 end
